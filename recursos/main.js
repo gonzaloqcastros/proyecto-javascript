b8e33290b6b35e63ebe5270793b93873b0a18a71
Gonzalo Quispe Castros <gonzaloqcastros@gmail.com>
2022-10-27 22:46:33 -0300

    adapte el contenido, y cree login

--- recursos/main.js
+++ recursos/main.js
@@ -1,4 +1,16 @@
-alert("Bienvenido a la calculadora de promedios");
+
+class Alumno{
+    constructor (id, nombreAlumno, nota1, nota2, nota3, nota4, promedio, aprobado){
+        this.id = id;
+        this.nombreAlumno = nombreAlumno;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.nota4 = nota4;
+        this.promedio = promedio;
+        this.aprobado = aprobado;
+    }
+}
 
 class Usuario {
     constructor(usuariocorrecto, contraseniacorrecta){
@@ -6,131 +18,102 @@ class Usuario {
         this.contraseniacorrecta = contraseniacorrecta;
     } 
 }
+const usuario01 = new Usuario("usuario", "1234")
 
-class Alumno{
-    constructor (nombreAlumno, promedio, aprobado){
-        this.nombreAlumno = nombreAlumno;
-        this.promedio = promedio;
-        this.aprobado = aprobado
+console.log (usuario01);
+
+let Alumnos = []
+
+
+
+function inicializarElementos() {
+    formularioLogin = document.getElementById(
+      "formularioLogin"
+    );
+    inputUsuario = document.getElementById("inputUsuario");
+    inputContraseña = document.getElementById("inputContraseña")
+    contenedorLogin = document.getElementById("contenedorLogin");
+
+    contenedorUsuario = document.getElementById("contenedorUsuario");
+    textoUsuario = document.getElementById("textoUsuario");
+  
+    botonLimpiarStorage = document.getElementById("limpiarStorage");
+    formulario = document.getElementById("formularioAgregarAlumno");
+    inputId = document.getElementById("inputId");
+    inputNombreAlumno = document.getElementById("inputNombreAlumno");
+    inputNota1 = document.getElementById("inputNota1");
+    inputNota2 = document.getElementById("inputNota2");
+    inputNota3 = document.getElementById("inputNota3");
+    inputNota4 = document.getElementById("inputNota4");
+    contenedorAlumnos = document.getElementById("contenedorAlumnos");
+  
+    botonesCerrarModalAgregarAlumno = document.getElementsByClassName(
+      "btnCerrarModalAgregarAlumno"
+    );
+    modalAddAlumno = document.getElementById("modalAddAlumno");
+    botonAgregarAlumno = document.getElementById("btnAgregarAlumno");
+    modal = new bootstrap.Modal(modalAddAlumno);
+  }
+
+
+
+  function inicializarEventos() {
+    formulario.onsubmit = (event) => validarFormulario(event);
+    formularioLogin.onsubmit = (event) => identificarUsuario(event);
+    botonLimpiarStorage.onclick = eliminarStorage;
+    botonAgregarAlumno.onclick = abrirModalAgregarAlumno;
+  
+    for (const boton of botonesCerrarModalAgregarAlumno) {
+      boton.onclick = cerrarModalAgregarAlumno;
     }
-}
-
-const usuario01 = new Usuario("usuario", "contraseña");
-
-console.log(usuario01)
-
-let AlumnosEvaluados = []
-
-let usuario = document.getElementById("inputUsuario").value;
-let contrasenia = Document.getElementById("inputContraseña").value;
-
-
-if (usuario === usuario01.usuariocorrecto && contrasenia === usuario01.contraseniacorrecta) {
-    alert("Bienvenido " + usuario)
-
-    let cantidadAlumnos = parseInt(prompt("Ingrese la cantidad de alumnos"))
-
-    for(let i = 0; i < cantidadAlumnos ; i ++) {
-
-        
-
-        let nombreAlumno = prompt("Ingrese el nombre del alumno");
-        let nota1 = parseInt(prompt("Ingrese la nota 1: "));
-        let nota2 = parseInt(prompt("Ingrese la nota 2: "));
-        let nota3 = parseInt(prompt("Ingrese la nota 3: "));
-        let nota4 = parseInt(prompt("Ingrese la nota 4: "));
-
-        let promedio = calculoPromedio (nota1, nota2, nota3, nota4);
-
-        alert("El promedio de " + nombreAlumno + " es " + promedio)
-
-        let aprueba = aprobado (promedio);
-        if(promedio >= 7 && promedio <= 10) {
-            alert(nombreAlumno + " esta aprobado");
-        }else if (promedio >= 0 && promedio < 7) {
-            alert(nombreAlumno + " no esta aprobado");
-        }else{
-            alert ("Los valores ingresados son incorrectos");
-        }
-
-        const Alumnno01 = new Alumno(nombreAlumno, promedio, aprueba);
-
-        AlumnosEvaluados.push (Alumnno01);
+  }
+  
+  function abrirModalAgregarAlumno() {
+    if (usuario) {
+      modal.show();
+    } else {
+      alert("Identifíquese antes de agregar un Alumno");
     }
+  }
+  
+  function cerrarModalAgregarAlumno() {
+    formulario.reset();
+    modal.hide();
+  }
+  
+  function eliminarStorage() {
+    localStorage.clear();
+    usuario = "";
+    Alumnos = [];
+    mostrarFormularioLogin();
+    mostrarAlumnos();
+  }
+  
+  function identificarUsuario(event) {
+    if (inputContraseña.value === usuario01.contraseniacorrecta && inputUsuario.value === usuario01.usuariocorrecto){
+    event.preventDefault();
+    usuario = inputUsuario.value;
+    formularioLogin.reset();
+    actualizarUsuarioStorage();
+    mostrarTextoUsuario();
 }else {
-    alert("Usuario o Contraseña incorrectos");
+    alert("Contraseña o usuario incorrectos");
 }
-
-for(let datos of AlumnosEvaluados) {
-    console.log(datos);
-}
-
-function menu() {
-    alert("Desea continuar?")
-    let opcion = parseInt(prompt("Ingrese una opción: \n 1) Buscar alumno \n 2)Modificar Alumno \n 3) Salir"));
-    return opcion;
-}
-
-function buscarAlumno() {
-    let alumnoBuscar = prompt ("Ingrese el nombre del alumno: ");
-    let Alumno01 = AlumnosEvaluados.find(Alumno01 => Alumno01.nombreAlumno === alumnoBuscar);
-    console.log (Alumno01);
-}
-
-
-function modificarAlumno () {
-
-    class Alumno{
-        constructor (numeroAlumno, nombreAlumno, promedio, aprobado){
-            this.numeroAlumno = numeroAlumno;
-            this.nombreAlumno = nombreAlumno;
-            this.promedio = promedio;
-            this.aprobado = aprobado
-        }
-    }
-
-    let alumnoBuscar = prompt ("Ingrese el nombre del alumno: ");
-    let Alumno = AlumnosEvaluados.find(Alumno01 => Alumno01.nombreAlumno === alumnoBuscar);
-    let nombreAlumno = prompt ("Ingrese el nombre del alumno: ");
-    let nota1 = parseInt(prompt("Ingrese la nota 1: "));
-    let nota2 = parseInt(prompt("Ingrese la nota 2: "));
-    let nota3 = parseInt(prompt("Ingrese la nota 3: "));
-    let nota4 = parseInt(prompt("Ingrese la nota 4: "));
-    let promedio = calculoPromedio (nota1, nota2, nota3, nota4);
-    alert("El promedio de " + nombreAlumno + " es " + promedio);
-    let aprueba = aprobado (promedio);
-        if(promedio >= 7 && promedio <= 10) {
-            alert(nombreAlumno + " esta aprobado");
-        }else if (promedio >= 0 && promedio < 7) {
-            alert(nombreAlumno + " no esta aprobado");
-        }else{
-            alert ("Los valores ingresados son incorrectos");
-        }
-    let indice = AlumnosEvaluados.indexOf(Alumno)
-    let alumnoModificado = new Alumno(nombreAlumno, promedio, aprueba)
-    AlumnosEvaluados.splice(indice, 1, alumnoModificado)
-    console.log(AlumnosEvaluados)
-}
-
-function salir() {
-    alert("¡Hasta pronto!");
-}
-
-let opcion = menu();
-switch (opcion) {
-    case 1:
-        buscarAlumno();
-        break;
-    case 2:
-        modificarAlumno();
-        break;
-    case 3:
-        salir();
-        break;
-}
-
-
-function calculoPromedio(nota1, nota2, nota3, nota4) {
+  }
+  
+  function mostrarTextoUsuario() {
+    contenedorLogin.hidden = true;
+    contenedorUsuario.hidden = false;
+    textoUsuario.innerHTML += ` ${usuario}`;
+  }
+  
+  function mostrarFormularioLogin() {
+    contenedorLogin.hidden = false;
+    contenedorUsuario.hidden = true;
+    textoUsuario.innerHTML = ``;
+  }
+  
+  function calculoPromedio(nota1, nota2, nota3, nota4) {
     let promedio = (nota1 + nota2 + nota3 + nota4) / 4;
     return promedio;
 }
@@ -143,5 +126,160 @@ function aprobado(promedio) {
     }
 }
 
-console.log(AlumnosEvaluados)
-Bienvenida.innerText = `Bienvenido ${usuario}!`;
+  function validarFormulario(event) {
+    event.preventDefault();
+    let idAlumno = inputId.value;
+    let nombreAlumno = inputNombreAlumno.value;
+    let nota1 = parseFloat(inputNota1.value);
+    let nota2 = parseFloat(inputNota2.value);
+    let nota3 = parseFloat(inputNota3.value);
+    let nota4 = parseFloat(inputNota4.value);
+    let promedio = calculoPromedio (nota1, nota2, nota3, nota4)
+    let aprueba = aprobado (promedio);
+    if(promedio >= 7 && promedio <= 10) {
+        aprueba = "esta aprobado";
+    }else if (promedio >= 0 && promedio < 7) {
+        aprueba = "no esta aprobado";
+    }else{
+        aprueba = "Los valores ingresados son incorrectos";
+    }
+
+    const idExiste = Alumnos.some((Alumno) => Alumno.id === idAlumno);
+    if (!idExiste) {
+      let alumno = new Alumno(
+        idAlumno,
+        nombreAlumno,
+        nota1,
+        nota2,
+        nota3,
+        nota4,
+        promedio,
+        aprueba
+      );
+  
+      Alumnos.push(alumno);
+      formulario.reset();
+      actualizarAlumnosStorage();
+      mostrarAlumnos();
+      mostrarMensajeConfirmacion(
+        `El Alumno ${nombreAlumno} fue agregado exitosamente`,
+        "info"
+      );
+    } else {
+      alert("El id ya existe");
+    }
+  }
+  
+  function confirmarEliminacion(idAlumno) {
+    Swal.fire({
+      icon: "question",
+      title: "¿Estas seguro que quieres eliminar el Alumno?",
+      showCancelButton: true,
+      confirmButtonText: "Eliminar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        eliminarAlumno(idAlumno);
+      }
+    });
+  }
+  
+  function eliminarAlumno(idAlumno) {
+    let columnaBorrar = document.getElementById(`columna-${idAlumno}`);
+    let indiceBorrar = Alumnos.findIndex(
+      (Alumno) => Number(Alumno.id) === Number(idAlumno)
+    );
+  
+    let nombreAlumnoEliminado = Alumnos[indiceBorrar].nombreAlumno;
+    Alumnos.splice(indiceBorrar, 1);
+    columnaBorrar.remove();
+    actualizarAlumnosStorage();
+    mostrarMensajeConfirmacion(
+      `El Alumno ${nombreAlumnoEliminado} fue eliminado exitosamente`,
+      "danger"
+    );
+  }
+  
+  
+  function mostrarAlumnos() {
+    contenedorAlumnos.innerHTML = "";
+    Alumnos.forEach((Alumno) => {
+      let column = document.createElement("div");
+      column.className = "col-md-4 mt-3";
+      column.id = `columna-${Alumno.id}`;
+      column.innerHTML = `
+              <div class="card">
+                  <div class="card-body">
+                  <p class="card-text">ID:
+                      <b>${Alumno.id}</b>
+                  </p>
+                  <p class="card-text">Nombre:
+                      <b>${Alumno.nombreAlumno}</b>
+                  </p>
+                  <p class="card-text">Promedio:
+                      <b>${Alumno.promedio}</b>
+                  </p>
+                  <p class="card-text">Aprobado:
+                      <b>${Alumno.aprobado}</b>
+                  </p>
+                  </div>
+                  <div class="card-footer">
+                    <button class="btn btn-danger" id="botonEliminar-${Alumno.id}" >Eliminar</button>
+                  </div>
+              </div>`;
+  
+      contenedorAlumnos.append(column);
+  
+      let botonEliminar = document.getElementById(`botonEliminar-${Alumno.id}`);
+      botonEliminar.onclick = () => confirmarEliminacion(Alumno.id);
+    });
+  }
+  
+  function actualizarAlumnosStorage() {
+    let alumnosJSON = JSON.stringify(Alumnos);
+    localStorage.setItem("Alumnos", alumnosJSON);
+  }
+  
+  function actualizarUsuarioStorage() {
+    localStorage.setItem("usuario", usuario);
+  }
+  
+  function obtenerAlumnosStorage() {
+    let alumnosJSON = localStorage.getItem("Alumnos");
+    if (alumnosJSON) {
+      Alumnos = JSON.parse(alumnosJSON);
+      mostrarAlumnos();
+    }
+  }
+  
+  function obtenerUsuarioStorage() {
+    let usuarioAlmacenado = localStorage.getItem("usuario");
+    if (usuarioAlmacenado) {
+      usuario = usuarioAlmacenado;
+      mostrarTextoUsuario();
+    }
+  }
+  
+  function mostrarMensajeConfirmacion(mensaje, clase) {
+    Toastify({
+      text: mensaje,
+      duration: 30000,
+      close: true,
+      gravity: "top",
+      position: "right",
+      className: clase
+    }).showToast();
+  }
+  
+  function main() {
+    inicializarElementos();
+    inicializarEventos();
+    obtenerAlumnosStorage();
+    obtenerUsuarioStorage();
+  }
+  
+
+
+  main();
+
+
